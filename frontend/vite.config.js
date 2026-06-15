b5a3c6a3525a15cfc1a3697bd4f9180f77977e8a
MH23001 <mh23001@ues.edu.sv>
2026-06-14 21:33:42 -0600

    Update proxy path in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/tareas*': {
+      '/tareas': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       }
